db6eaf0f5c003ad33c39c94cfbedc22781ec2a3a
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-08-23 01:22:50 +0200

    Add changes for dark theme

---
+++ Downloads/Files/Files/home/coder/project/reactlab/src/ThemeContext.js
@@ -0,0 +1,23 @@
+import React, {createContext, useState, useContext} from 'react';
+
+
+
+const ThemeContext = createContext (undefined);
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light")
+
+    return (
+        <ThemeContext.Provider 
+        value={{
+            theme,
+            toggleTheme: () => setTheme(theme=== "light" ? "dark" : "light"), 
+        }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    )
+    
+};
+ 
+export const useTheme = () => useContext(ThemeContext);
